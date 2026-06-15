e5825b2b019691bcc0b6ba2929561c5ff6773bd2
Michael <70063789+MichaelDavidSykes@users.noreply.github.com>
2026-06-15 14:58:16 +0100

    Remove stale landing product services

--- src/app/landing-page/landing-page.component.spec.ts
+++ src/app/landing-page/landing-page.component.spec.ts
@@ -1,5 +1,9 @@
+import { NO_ERRORS_SCHEMA, PLATFORM_ID } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { GlobalSnackbarService } from '../shared/global-snackbar/global-snackbar.service';
 import { LandingPageComponent } from './landing-page.component';
 
 describe('LandingPageComponent', () => {
@@ -8,7 +12,19 @@ describe('LandingPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LandingPageComponent]
+      declarations: [LandingPageComponent],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: PLATFORM_ID, useValue: 'server' },
+        {
+          provide: GlobalSnackbarService,
+          useValue: jasmine.createSpyObj<GlobalSnackbarService>('GlobalSnackbarService', ['open'])
+        }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
     
