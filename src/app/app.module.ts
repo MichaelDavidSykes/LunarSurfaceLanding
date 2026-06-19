81367b239bf617dd740b2ffc227b813b3f2d2fa9
Michael <70063789+MichaelDavidSykes@users.noreply.github.com>
2026-06-19 20:17:44 +0100

    Remove stale landing section code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { AgCharts } from 'ag-charts-angular';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { MarkdownModule } from 'ngx-markdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,8 +12,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSliderModule } from '@angular/material/slider';
 import { CommonModule } from '@angular/common';
-import { NgChartsModule } from 'ng2-charts';
-import { NgxEchartsModule } from 'ngx-echarts';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { InteractiveGlobeComponent } from './interactive-globe/interactive-globe.component';
 import { ApiOverviewComponent } from './api-overview/api-overview.component';
@@ -49,15 +44,7 @@ import { TermsComponent } from './terms/terms.component';
     MatIconModule,
     MatProgressBarModule,
     MatSliderModule,
-    NgChartsModule,
-    AgCharts,
-    NgxChartsModule,
-
-    NgxEchartsModule.forRoot({
-      echarts: () => import('echarts'),
-    }),
     MatSnackBarModule,
-    MarkdownModule.forRoot(),
   ],
   providers: [
     provideClientHydration(),
--- src/types/d3-sankey.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'd3-sankey' {
-  export function sankey(): any;
-  export function sankeyJustify(node: any, n: number): number;
-  export function sankeyLinkHorizontal(): any;
-}
